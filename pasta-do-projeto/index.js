dda4df38e7236e096685f84e8eaad734091bfb73
José Wytalo <112438631+JwapWytalo@users.noreply.github.com>
2023-04-18 21:09:12 -0300

    Add files via upload

--- pasta-do-projeto/index.js
+++ pasta-do-projeto/index.js
@@ -3,29 +3,11 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express()
-const port = 8080
+const port = 3000
 
 app.use(express.static('public'));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/PostForm.html')
-})
-
-app.post('/mirror', (req, res) => {
-  const firstName = req.body.FirstName;
-  const lastName = req.body.LastName;
-
-  const html = fs.readFileSync(path.join(__dirname, 'mirror.html'), 'utf8');
-
-  const updatedHtml = html
-    .replace('{firstName}', firstName)
-    .replace('{lastName}', lastName);
-
-  res.send(updatedHtml);
-});
-
 
 
 
